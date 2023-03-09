ab3ab67ffc94c0a827fdd7e959c33510671501fa
Leonardo Manuel Grau Vargas <leonardo.grau@outlook.com.pe>
2023-03-09 17:28:12 -0500

    feat: finished sidenav

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,18 +1,37 @@
 var navbar = document.getElementById("navbar");
+var sideNavbar = document.getElementById("side-navbar");
+
+var _closeSideNav = document.getElementById("closeSideNav");
+var _openSideNav = document.getElementById("openSideNav");
 
 function changeNavColorOnScroll() {
   pixelsScrolledOnYAxis = window.scrollY;
   if(pixelsScrolledOnYAxis > 1) {
     navbar.classList.remove("not-scrolledY");
     navbar.classList.add("scrolledY");
+    sideNavbar.classList.remove("not-scrolledY");
+    sideNavbar.classList.add("scrolledY");
   } else {
     navbar.classList.remove("scrolledY");
     navbar.classList.add("not-scrolledY");
+    sideNavbar.classList.remove("scrolledY");
+    sideNavbar.classList.add("not-scrolledY");
   }
 }
 
+function openSideNav() {
+  sideNavbar.style.width = "85vw";
+}
+
+function closeSideNav() {
+  sideNavbar.style.width = "0";
+}
+
+
 function main() {
   window.onscroll = () => { changeNavColorOnScroll(); }
+  _closeSideNav.onclick = () => { closeSideNav(); console.log("close it"); }
+  _openSideNav.onclick = () => { openSideNav(); console.log("open it"); }
 }
 
 main();
